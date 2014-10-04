0d26df596d9bbbd7c1e8a8c2adf943e48b4f03b1
unknown <ajwieme@gmail.com>
2014-10-04 17:17:34 -0500

    pushing first 2 test functions

---
+++ TfTwoLoops.rb
@@ -0,0 +1,48 @@
+# This is a fake calculation simply used to test the efficiency of 2 different strategies 
+# for looping over data that will have calculations be run on it.
+# 
+# This strategy is a little slower with smaller amounts of words
+# But as the content gets larger, runtime increases only slightly
+#
+class TfTest
+	def initialize(content)
+		@content = content
+	end
+
+	def time_method(method)
+	  beginning_time = Time.now
+	  self.send(method)
+	  end_time = Time.now
+	  puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
+	end
+
+	def get_tf
+		@tf_hash = Hash.new
+		# The amount of times a term appears 
+		# In the document divided by the total 
+		# Amount of tokens in the document.
+		@quantity = self.tokenize.length
+		@tokens.each do |term|
+			if @tf_hash[term]
+				@tf_hash[term] += 1
+			else
+				@tf_hash[term] = 1
+			end
+		end
+
+		@tf_hash.each do |term, weight|
+			@tf_hash[term] = Math.log(@quantity/@tf_hash[term])
+		end
+		return @tf_hash
+	end
+
+	def tokenize
+		@tokens = @content.gsub(/[^a-zA-Z0-9 - ' _]/, "")
+		@tokens = @tokens.split(' ')
+		return @tokens
+	end
+end
+
+x = TfTest.new("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,")
+tf_scores = x.time_method(:get_tf)
+puts tf_scores
